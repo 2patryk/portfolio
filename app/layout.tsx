3d39e260b4adc7f4fb236f3a1650aedf77561e3d
Patryk Ordon <patrykordon@gmail.com>
2024-08-27 18:38:08 +0200

    feat: update ga4

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { useGlobalStore } from "@/utils/global.store";
 // import { useTimer } from "use-timer";
 import Header from "@/components/molecules/Header/Header";
 import { useEffect } from "react";
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 
 const syne = Syne({
@@ -65,7 +65,7 @@ export default function RootLayout({
           </ThemeProvider>
         </StyledComponentsRegistry>
       </body>
-      <GoogleTagManager gtmId="GTM-KC2B5VWR" />
+      <GoogleAnalytics gaId="G-VDMMJE25JQ" />
     </html>
   );
 }
