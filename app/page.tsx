79f09dd2e88e3b86f32aa791e6fbd917291d74e3
Patryk Ordon <patrykordon@gmail.com>
2024-08-31 00:44:59 +0200

    feat: add og image for home

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   ...globalCopy.meta.index,
+  openGraph: {
+    images: ["/images/og-image.png"],
+  },
 };
 
 export default function Home() {
